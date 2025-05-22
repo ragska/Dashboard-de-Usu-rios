88cc3118ae9f002a991ed8c36fadcca347a7e434
RAFAELA VALENTINA COSTA KAGE <rafaela.kage@etec.sp.gov.br>
2025-05-22 15:46:17 -0300

    Commit inici\l

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import{ useEffect,useState } from 'react';
+import UserCard from './components/UserCard';
+import './App.css';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/people')
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.error('Erro ao buscar usuários:', err));
+  }, []);
+
+  return(
+    <div className="App">
+      <h1>Dashboard de Usuários</h1>
+      <p>Total de usuários: {users.length}</p>
+      <div className="user-container">
+        {users.map((user) => (
+          <UserCard key= {user.id} user={user} />
+        ))}
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,20 @@
+import './UserCard.css';
+
+function UserCard({user}) {
+
+    return(
+
+        <div className="user-card">
+
+            <img src={user.avatar} alt={`${user.firstName} avatar`} />
+            <h3>{user.firstName} {user.lastName}</h3>
+            <p>{user.email}</p>
+            <small>{user.address}</small>
+
+        </div>
+
+    );
+
+}
+
+export default UserCard;
