37c5828cc5f250a277c9cecace479cd9692ec3ed
RAFAELA VALENTINA COSTA KAGE <rafaela.kage@etec.sp.gov.br>
2025-05-29 15:48:15 -0300

    ta igual o pdf mas n funfa

---
+++ backend/generate.js
@@ -0,0 +1,28 @@
+import { faker } from 'faker-js/faker/locale/pt_BR';
+import lodash from 'lodash';
+import fs from 'fs';
+
+const peoples = lodash.times(50, function(n){
+
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();    
+
+    return{
+
+        id: n+1,
+        firstname: firstName,
+        lastname: lastName,
+        avatar: faker.image.avatar(),
+        address: faker.location.streetAddress(),
+        email: faker.internet.email({firstName: firstName.toLowerCase(), lastName: lastName.toLowerCase()})
+
+    }
+
+});
+
+const data = {};
+data.peoples = peoples;
+fs.writeFile('db.json', JSON.stringify(data), (err) => {
+    if(err) throw err;
+    console.log('finalizado...');
+} );
