03eb06043b64e371712d511e5a185d2a40a03b70
ragska <132146709+ragska@users.noreply.github.com>
2025-06-12 14:43:41 -0300

    comentei

--- src/components/UserDetail.jsx
+++ src/components/UserDetail.jsx
@@ -1,31 +1,37 @@
-import { useParams, useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import './UserDetail.css'; // Import your CSS file for styling
+import { useParams, useNavigate } from 'react-router-dom'; //importa os hooks necessários do react-router-dom
+import { useEffect, useState } from 'react'; //importa os hooks necessário do react
+import './UserDetail.css'; // importa a estilização
 
 function UserDetail() {
-  const { id } = useParams();
-  const navigate = useNavigate();
-  const [user, setUser] = useState(null);
+  const { id } = useParams(); //pega o valor do :id na URL
+  const navigate = useNavigate();// cria função de rota
+  const [user, setUser] = useState(null); //determina variavel user de valor alterável pela função setUser
 
+  //acessa os dados de um usuário/peoples específico do backend
   useEffect(() => {
     fetch(`http://localhost:3001/peoples/${id}`)
       .then(res => res.json())
       .then(data => setUser(data))
-      .catch(err => console.error('Erro ao buscar usuário:', err));
+      .catch(err => console.error('Erro ao buscar usuário:', err)); //retorna mensagem de erro se não achar 
   }, [id]);
 
   if (!user) {
     return <p>Carregando usuário...</p>;
   }
 
+  //retorna parte visivel
   return (
     <div className="user-detail" style={{ padding: '20px' }}>
+
+      {/*botão para navegar para a rota anterior (dashboard)*/}
         <button onClick={() => navigate(-1)} style={{ marginBottom: '20px' }}>
             Voltar
         </button>
 
+      {/*título da div*/}
       <h2>Detalhes do Usuário</h2>
 
+      {/*informações do usuários: imagem, nome, email e endereço*/}
       <img src={user.avatar} alt={`${user.firstName} avatar`} />
       <p><strong>Nome:</strong> {user.firstName} {user.lastName}</p>
       <p><strong>Email:</strong> {user.email}</p>
